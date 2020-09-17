164ccc29c35aa4b11e250c37d462a6014ee38e04
kachi <onyedikachinwosu@rocketmail.com>
2020-09-17 15:11:42 +0100

    Updates

---
+++ precache-manifest.a66ea8746755118529078204f88f63b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1b83f91a1f062c57ad759eebc6773dc",
+    "url": "/robo-friends/index.html"
+  },
+  {
+    "revision": "7aa7af5eb5ef9ec87634",
+    "url": "/robo-friends/static/css/2.7a3f122f.chunk.css"
+  },
+  {
+    "revision": "fc5a7472476665bfd6ff",
+    "url": "/robo-friends/static/css/main.fb4c2e0c.chunk.css"
+  },
+  {
+    "revision": "7aa7af5eb5ef9ec87634",
+    "url": "/robo-friends/static/js/2.94842557.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/robo-friends/static/js/2.94842557.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc5a7472476665bfd6ff",
+    "url": "/robo-friends/static/js/main.ef668981.chunk.js"
+  },
+  {
+    "revision": "b7276070548a11fb737e",
+    "url": "/robo-friends/static/js/runtime-main.17f36485.js"
+  }
+]);
